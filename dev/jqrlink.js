aab4d641a59fe4cab1b5e2191512ac0ab742da86
m.dinman <m.dinman@agurustudio.com>
2013-06-26 16:25:01 +0300

    initial

---
+++ dev/jqrlink.js
@@ -0,0 +1,27 @@
+/**
+ * Description: jQuery QR picture generator.
+ * Author: ant777
+ * Date: 26.06.13
+ * Time: 11:17
+ */
+(function( $ ) {
+
+    $.fn.jqrlink = function( options ) {
+
+        var defaults = {
+
+        };
+        // Build main options before element iteration.
+        var opts = $.extend( {}, defaults, options );
+
+        return this.each(function() {
+            var $elf = $(this);
+            var p = $elf.wrap('<div/>').parent();
+            console.log($elf[0].href)
+            p.append('<img src="https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl='+$elf[0].href+'"/>');
+        });
+    };
+
+
+}( jQuery ));
+
