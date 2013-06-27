7362031cc4579a2c5fbbe4d7c8bd7f10e2791eb3
a.ivanov <a.ivanov@richbrains.net>
2013-06-27 11:02:06 +0300

    rev 1 ver

--- dev/jqrlink.js
+++ dev/jqrlink.js
@@ -8,19 +8,52 @@
 
     $.fn.jqrlink = function( options ) {
 
+        var $elf = this;
+        if( $elf.length > 1 ){
+            return $elf.each(function() {
+                $(this).jqrlink ( options )
+            })
+
+        }
+        console.log(this)
         var defaults = {
+            display:    'hover',    // always | click |onButton
+            fx:         'fade',     // flip | none | zoom
+            nostyle:    false,       //true
+            provider: {
+                restUrl: "https://chart.googleapis.com/chart?"
+            }
 
         };
         // Build main options before element iteration.
         var opts = $.extend( {}, defaults, options );
 
-        return this.each(function() {
-            var $elf = $(this);
-            var p = $elf.wrap('<div/>').parent();
-            console.log($elf[0].href)
-            p.append('<img src="https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl='+$elf[0].href+'"/>');
-        });
+        var jqr = (function(){
+            var wrap, qr;
+
+            function bindEvt( evt ){
+
+                switch(evt){
+                    case 'hover':
+                        $elf.on('mouseenter')
+                }
+            }
+
+            return {
+                init: function(){
+                    wrap = $elf.wrap('<div/>').parent();
+                    console.log($elf[0].href);
+                    qr = $('<img src="https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl='+$elf[0].href+'"/>').appendTo(wrap);
+                    bindEvt( opts.display )
+                }
+
+            }
+        }());
+        jqr.init();
+        return $elf;
     };
+
+
 
 
 
